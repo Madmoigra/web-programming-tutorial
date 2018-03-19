7f1a5a94807791d802494b8876115f3bcccb0f75
Cristian <moigradancristian@gmail.com>
2018-03-19 23:05:10 +0200

    oare merge?

--- js/first-script.js
+++ js/first-script.js
@@ -1,19 +1,20 @@
 console.info("page loaded");
-var age= 24;
-var name= "cristian";
-var employed= true;
-console.info ("numele:" + name);
-console.info ("varsta:" + age);
-console.info ("angajat:" + employed);
+var age = 24;
+var name = "cristian";
+var employed = true;
+console.info("numele:" + name);
+console.info("varsta:" + age);
+console.info("angajat:" + employed);
 
 function getinfo(name) {
-    var msg= "Hello" +name",+ "welcome at FasttrackIT";
+    var msg = "Hello" + name + ", welcome at FasttrackIT";
     return msg;
 }
 
 function addApples(firstNumber, secondNumber) {
     var count = firstNumber + secondNumber;
-    return count -1;
-
-
 }
+
+var rezultat = addApples(4, 6);
+
+console.info(rezultat);
