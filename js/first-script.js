d3a9f13a7dce30b81a1c6677b893ce173ff0f842
Cristian <moigradancristian@gmail.com>
2018-03-19 23:04:15 +0200

    update title

---
+++ js/first-script.js
@@ -0,0 +1,19 @@
+console.info("page loaded");
+var age= 24;
+var name= "cristian";
+var employed= true;
+console.info ("numele:" + name);
+console.info ("varsta:" + age);
+console.info ("angajat:" + employed);
+
+function getinfo(name) {
+    var msg= "Hello" +name",+ "welcome at FasttrackIT";
+    return msg;
+}
+
+function addApples(firstNumber, secondNumber) {
+    var count = firstNumber + secondNumber;
+    return count -1;
+
+
+}
